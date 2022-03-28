1c5821e8dbdd3f67d3f87102c854d559fbadbc90
HamedKarimi7 <hmdkrm.rtj@gmail.com>
2022-03-28 15:13:38 +0200

    practice1 int Array

--- src/main/java/se/lexicon/hamed/App.java
+++ src/main/java/se/lexicon/hamed/App.java
@@ -4,10 +4,19 @@ package se.lexicon.hamed;
  * Hello world!
  *
  */
-public class App 
+/*
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+}
+*/
+
+
+public class App {
+    public static void main(String[] args) {
+
     }
 }
---
+++ src/main/java/se/lexicon/hamed/Array.java
@@ -0,0 +1,15 @@
+package se.lexicon.hamed;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] numberArray= new int[2]; // {11, 12, 15}
+        numberArray [0] = 11;
+        numberArray [1] = 12;
+        System.out.println(numberArray[0]);
+        System.out.println(numberArray[1]);
+
+    }
+}
